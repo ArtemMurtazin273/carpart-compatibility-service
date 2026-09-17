5fa09b4319684121c2d32fd7e1d4489d20a3e553
artemmurtazin27 <artemmurtazin27@gmail.com>
2026-09-17 15:45:04 +0300

    implemented manufacturer list view using login required mixin in views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
+from catalog.forms import ManufacturerSearchForm
 from catalog.models import Car, Manufacturer, Part, PartCategory
 
 
@@ -26,3 +29,23 @@ def index(request):
     }
 
     return render(request, "catalog/index.html", context=context)
+
+
+class ManufacturerListView(LoginRequiredMixin, generic.ListView):
+    model = Manufacturer
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = ManufacturerSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Manufacturer.objects.all()
+        form = ManufacturerSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
