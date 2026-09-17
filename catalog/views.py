580479f844f4ded703d2f2d89e22714652c3411e
artemmurtazin27 <artemmurtazin27@gmail.com>
2026-09-17 15:52:13 +0300

    implemented part list view using login required mixin in views.py

--- catalog/views.py
+++ catalog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from catalog.forms import ManufacturerSearchForm, CarSearchForm, CategorySearchForm
+from catalog.forms import ManufacturerSearchForm, CarSearchForm, CategorySearchForm, PartSearchForm
 from catalog.models import Car, Manufacturer, Part, PartCategory
 
 
@@ -151,3 +151,23 @@ class PartCategoryDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = PartCategory
     template_name = "catalog/category_confirm_delete.html"
     success_url = reverse_lazy("catalog:category-list")
+
+
+class PartListView(LoginRequiredMixin, generic.ListView):
+    model = Part
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = PartSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Part.objects.select_related("manufacturer", "category")
+        form = PartSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
