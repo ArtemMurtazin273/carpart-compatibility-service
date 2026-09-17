5dc5dd7fe2efe3b22a908bb07ec727d2ad8050ff
artemmurtazin27 <artemmurtazin27@gmail.com>
2026-09-17 15:32:23 +0300

    implemented mechanic creation form

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+
+from catalog.models import Mechanic
 
 
 def validate_license_number(license_number):
@@ -9,3 +12,17 @@ def validate_license_number(license_number):
     if not license_number[3:].isdigit():
         raise ValidationError("Last 5 characters must be digits.")
     return license_number
+
+
+class MechanicCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Mechanic
+        fields = UserCreationForm.Meta.fields + (
+            "license_number",
+            "first_name",
+            "last_name",
+            "email",
+        )
+
+    def clean_license_number(self):
+        return validate_license_number(self.cleaned_data["license_number"])
