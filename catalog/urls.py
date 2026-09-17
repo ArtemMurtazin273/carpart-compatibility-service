38890a97675271ca929eb6c7c14c4dc320a578f1
artemmurtazin27 <artemmurtazin27@gmail.com>
2026-09-17 16:03:53 +0300

    add manufacturer paths to urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,34 @@ from django.urls import path
 
 from catalog.views import (
     index,
+    ManufacturerListView,
+    ManufacturerCreateView,
+    ManufacturerUpdateView,
+    ManufacturerDeleteView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list",
+    ),
+    path(
+        "manufacturers/create/",
+        ManufacturerCreateView.as_view(),
+        name="manufacturer-create",
+    ),
+    path(
+        "manufacturers/<int:pk>/update/",
+        ManufacturerUpdateView.as_view(),
+        name="manufacturer-update",
+    ),
+    path(
+        "manufacturers/<int:pk>/delete/",
+        ManufacturerDeleteView.as_view(),
+        name="manufacturer-delete",
+    ),
 ]
 
 app_name = "catalog"
