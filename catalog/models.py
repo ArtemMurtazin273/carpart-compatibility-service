7bb167670bd1f7b0f3c3caa477a6b692fd4763bc
artemmurtazin27 <artemmurtazin27@gmail.com>
2026-09-17 15:13:18 +0300

    add mechanic class inherits from abstract user to models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Mechanic(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "mechanic"
+        verbose_name_plural = "mechanics"
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
