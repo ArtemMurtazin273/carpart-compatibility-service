0db3e8c07c7ad00481538ed4a6d129efe2fdaad8
artemmurtazin27 <artemmurtazin27@gmail.com>
2026-09-17 15:49:54 +0300

    implemented car delete view using login required mixin in views.py

--- catalog/views.py
+++ catalog/views.py
@@ -104,3 +104,8 @@ class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
     fields = "__all__"
     success_url = reverse_lazy("catalog:car-list")
+
+
+class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Car
+    success_url = reverse_lazy("catalog:car-list")
